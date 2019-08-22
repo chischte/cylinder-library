0ff31d7b5c27343d47d5efc8d6d7acff4885de0f
realslimshady <chischte@gmx.ch>
2019-08-22 14:01:24 +0200

    cleanup work

--- Cylinder.cpp
+++ Cylinder.cpp
@@ -10,46 +10,54 @@
 #include "Arduino.h"
 #include "Cylinder.h"
 
-Cylinder::Cylinder(int pin) {
-    pinMode(pin, OUTPUT);
-    _pin = pin;
+Cylinder::Cylinder(int pin)
+{
+  pinMode(pin, OUTPUT);
+  _pin = pin;
 }
 //***************************************************************************
 //LIBRARY FUNCTIONS:
 //***************************************************************************
-void Cylinder::stroke(int push_time, int release_time) {
-    if (_stroke_completed == true) //RESET TIMER ONCE AT BEGINNING
-            {
-        digitalWrite(_pin, HIGH);
-        _state = HIGH;
-        _prev_time = millis();
-        _stroke_completed = false;
+void Cylinder::stroke(int push_time, int release_time)
+{
+  if (_stroke_completed == true) //RESET TIMER ONCE AT BEGINNING
+  {
+    digitalWrite(_pin, HIGH);
+    _state = HIGH;
+    _prev_time = millis();
+    _stroke_completed = false;
+  }
+  if (millis() - _prev_time > push_time)
+  {
+    digitalWrite(_pin, LOW);
+    _state = LOW;
+    if (millis() - _prev_time > push_time + release_time)
+    {
+      _stroke_completed = true; //TRIGGERS NEXT CYCLE STATE
     }
-    if (millis() - _prev_time > push_time) {
-        digitalWrite(_pin, LOW);
-        _state = LOW;
-        if (millis() - _prev_time > push_time + release_time) {
-            _stroke_completed = true; //TRIGGERS NEXT CYCLE STATE
-        }
-    }
+  }
 }
 //***************************************************************************
-bool Cylinder::stroke_completed() {
-    return _stroke_completed;
+bool Cylinder::stroke_completed()
+{
+  return _stroke_completed;
 }
 //***************************************************************************
-void Cylinder::toggle() {
-    _state = !_state;
-    digitalWrite((_pin), _state);
+void Cylinder::toggle()
+{
+  _state = !_state;
+  digitalWrite((_pin), _state);
 }
 //***************************************************************************
-void Cylinder::set(bool set_state) {
-    digitalWrite(_pin, set_state);
-    _state = set_state;
+void Cylinder::set(bool set_state)
+{
+  digitalWrite(_pin, set_state);
+  _state = set_state;
 }
 //***************************************************************************
-bool Cylinder::request_state() {
-    _state = (digitalRead(_pin));
-    return _state;
+bool Cylinder::request_state()
+{
+  _state = (digitalRead(_pin));
+  return _state;
 }
 //***************************************************************************
--- Cylinder.h
+++ Cylinder.h
@@ -12,20 +12,21 @@
 
 #include "Arduino.h"
 
-class Cylinder {
+class Cylinder
+{
 public:
-    Cylinder(int pin);
-    void stroke(int push_time, int release_time);
-    void toggle();
-    void set(bool set_state);
-    bool request_state();
-    bool stroke_completed();
+  Cylinder(int pin);
+  void stroke(int push_time, int release_time);
+  void toggle();
+  void set(bool set_state);
+  bool request_state();
+  bool stroke_completed();
 
 private:
-    int _pin;
-    bool _state;
-    unsigned long _prev_time;
-    bool _stroke_completed = true;
+  int _pin;
+  bool _state;
+  unsigned long _prev_time;
+  bool _stroke_completed = true;
 };
 
 #endif
