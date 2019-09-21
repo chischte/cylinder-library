3d3b88380bd1635a5d063e7e630bca4b44a33e8e
Michael Wettstein <chischte@gmx.ch>
2019-09-22 00:13:47 +0200

    Add example code

---
+++ cylinder_example_code/cylinder_example_code.ino
@@ -0,0 +1,32 @@
+#include <Cylinder.h> //https://github.com/chischte/cylinder-library.git
+
+const byte CYLINDER_VALVE_PIN = 4;
+
+// CREATE AN INSTANCE OF THE LIBRARY CLASS:
+Cylinder ExampleCylinder(CYLINDER_VALVE_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("EXIT SETUP");
+}
+
+void loop() {
+
+  ExampleCylinder.set(1); // push cylinder out
+  ExampleCylinder.set(0); // pull cylinder back
+  ExampleCylinder.toggle(); // change cylinder state
+
+  if (ExampleCylinder.request_state()) {
+    // if cylinder is extended, do stuff
+  }
+
+  if (!ExampleCylinder.request_state()) {
+    // if cylinder is pulled back, do stuff
+  }
+
+  ExampleCylinder.stroke(1500, 500); // (push time,pull time)
+
+  if (ExampleCylinder.stroke_completed()) {
+    // if cylinder has retracted do stuff}
+  }
+}
